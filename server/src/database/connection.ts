383d3f78c0779601fc76d0c8b4f1b1a2f3000c66
Jefferson Penna <jefferson.penna@accenture.com>
2020-06-07 10:31:43 -0300

    feat: web app, mobile app and server

---
+++ server/src/database/connection.ts
@@ -0,0 +1,15 @@
+import knex from 'knex'
+import path from 'path'
+
+const connection = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'database.sqlite')
+  },
+  seeds: {
+    directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+  },
+  useNullAsDefault: true
+})
+
+export default connection
